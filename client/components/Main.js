d02a4662fe6bd452308224b19ea6e5efbd448bd9
Daniel Lim <hbt@markhamboards.com>
2016-11-01 14:21:48 -0400

    restruct Infa
    restruct Infa

---
+++ client/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default const Main = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">React Redux Gallery App.</Link>
+        </h1>
+        {React.cloneElement(this.props.children, this.props)}
+      </div>
+    )
+  }
+});
+
+// export default Main;
