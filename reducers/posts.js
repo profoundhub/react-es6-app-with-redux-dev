cfefc504e59006fbc85f1abdd1c955f67f4b4cbb
Daniel Lim <hbt@markhamboards.com>
2016-11-01 15:05:37 -0400

    update reducers
    update reducers

--- reducers/posts.js
+++ reducers/posts.js
@@ -1,2 +1,6 @@
-
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+// NOTES: Take in actions, (what happened), a new copy of the store (current state), after work has been done. 
 export default posts;
