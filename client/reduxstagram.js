e6335c3ca4a15f58d356597ea0a8ff37eed7e7ab
Daniel Lim <hbt@markhamboards.com>
2016-11-01 14:09:57 -0400

    update init 2
    update init 2

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,13 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+// Import CSS
+import css from './styles/style.styl';
+const rootDiv = document.getElementById('root');
+
+// Import Components
+
+
+// import react router deps
+
+
+render(router, rootDiv);
