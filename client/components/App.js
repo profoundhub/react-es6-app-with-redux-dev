c4adf7ccb73fdd2cac18272b49af95113ea890fb
Daniel Lim <hbt@markhamboards.com>
2016-11-01 23:11:58 -0400

    + mapStateToProps
    mapStateToProps ++

--- client/components/App.js
+++ client/components/App.js
@@ -3,6 +3,12 @@ import { connect } from 'react-redux';
 import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    comments: state.comments
+  }
+}
 
 
 const App = connect(mapStateToProps, mapDispachToProps)(Main);
