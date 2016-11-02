7d3ffb004ce45cbafd770de13abf25cd57a722eb
Daniel Lim <hbt@markhamboards.com>
2016-11-02 00:32:13 -0400

    Start working on Comments
    Start working on Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,1 +1,27 @@
 import React from 'react';
+
+const Comments = React.createClass({
+
+/*
+  renderComment(comment, i) {
+    return (
+        
+        <div className="comment" key={ i }>
+            <p>
+                Hello
+            </p>
+        </div>
+    )
+  },
+*/
+
+  render() {
+    return (
+      <div className="comments">
+        Hello, I am a Comment!
+      </div>
+    )
+  }
+});
+
+export default Comments;
