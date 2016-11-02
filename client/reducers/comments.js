e6de39ad22ae14e8c32cc479af6d894fb5deebdb
Daniel Lim <hbt@markhamboards.com>
2016-11-02 16:40:04 -0400

    complete Redux Reducer Composition
    complete Redux Reducer Compositions

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -2,6 +2,7 @@ function postComments(state = [], action) {
 
   switch(action.type){
     case 'ADD_COMMENT':
+      console.log("Adding a comment");
       // return the new state with the new comment
         return [...state,{
           user: action.author,
@@ -9,23 +10,20 @@ function postComments(state = [], action) {
         }];
     case 'REMOVE_COMMENT':
       // we need to return the new state without the deleted comment
-      console.log("removing a comment");
+      console.log("Removing a comment");
       return [
         // from the start to the one we want to delete
-          ...state.slice(0,action.i),
+          ...state.slice(0, action.i),
         // after the deleted one, to the end
-          ...state.slice(action.i + 1)
+          ...state.slice(action.i +1)
       ]
     default:
       return state;
   }
-
 }
 
 function comments(state = [], action) {
-
   // console.log("the Comments may be going to change too ... ");
-
   if(typeof action.postId !== 'undefined') {
     return {
       // take the current state
@@ -35,7 +33,6 @@ function comments(state = [], action) {
     }
   }
   // console.log(state, action);
-
   return state;
 }
 
