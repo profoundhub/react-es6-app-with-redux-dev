b13ee313b2c0493151807a3dfec7f1b5c2d2ab7f
Daniel Lim <hbt@markhamboards.com>
2016-11-02 00:00:53 -0400

    Update JS
    Update JS

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,11 +1,11 @@
 function posts(state = [], action) {
-  console.log("the Post is going to change ... ")
+  // console.log("the Post is going to change ... ");
   console.log(state, action);
   return state;
 }
 
-// NOTES: Take in actions, (what happened), a new copy of the store (current state), after work has been done. 
+// NOTES: Take in actions, (what has happened), a new copy of the store (the current state), after work has been done. 
 
 export default posts;
 
-// $r.store.dispatch({type: 'INCREMENT_LIKES', index: 0});
+// $r.store.dispatch({ type: 'INCREMENT_LIKES', index: 0 });
