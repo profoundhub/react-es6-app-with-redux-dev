71816b01825b306d6a48688f3663e91893fb5766
Daniel Lim <hbt@markhamboards.com>
2016-11-02 16:33:14 -0400

    return the new state
    return the new state

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -7,6 +7,16 @@ function comments(state = [], action) {
 
   // console.log("the Comments may be going to change too ... ");
   
+
+
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // take the current state
+      ...state,
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
   // console.log(state, action);
 
   return state;
