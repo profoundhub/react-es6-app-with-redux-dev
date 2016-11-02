d4e21830b4ba473640a307fe830abc7b0ea1761f
Daniel Lim <hbt@markhamboards.com>
2016-11-02 17:26:41 -0400

    Issues: settle some, time for a major clean up
    settle some Issues, time for a major clean up

--- client/store.js
+++ client/store.js
@@ -11,4 +11,12 @@ const defaultState = { posts, comments };
 
 const store = createStore(rootReducer, defaultState);
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if(module.hot) {
+  module.hot.accept('./reducers/',() => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
+
 export default store;
