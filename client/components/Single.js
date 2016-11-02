7ee0600c540015abcb791d106830a971624b484a
Daniel Lim <hbt@markhamboards.com>
2016-11-02 00:16:19 -0400

    re building Singles

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
 
 const Single = React.createClass({
   render() {
     return (
       <div className="single-photo">
-        <p>I am the single</p>
+        
       </div>
     )
   }
 });
+
+// <p>I am the single</p>
 
 export default Single;
