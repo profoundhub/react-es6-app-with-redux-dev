bb2a4a011a1af4e915ec88cd3d29a73ef188ad1b
Daniel Lim <hbt@markhamboards.com>
2016-11-02 16:36:07 -0400

    code up Comments
    code up Comments

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,13 +1,30 @@
 function postComments(state = [], action) {
 
-  return state;
+  switch(action.type){
+    case 'ADD_COMMENT':
+      // return the new state with the new comment
+        return [...state,{
+          user: action.author,
+          text: action.comment
+        }];
+    case 'REMOVE_COMMENT':
+      // we need to return the new state without the deleted comment
+      console.log("removing a comment");
+      return [
+        // from the start to the one we want to delete
+          ...state.slice(0,action.i),
+        // after the deleted one, to the end
+          ...state.slice(action.i + 1)
+      ]
+    default:
+      return state;
+  }
+
 }
 
 function comments(state = [], action) {
 
   // console.log("the Comments may be going to change too ... ");
-  
-
 
   if(typeof action.postId !== 'undefined') {
     return {
