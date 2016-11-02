040e3fd0e49639640cccf1bca2e1445866670f11
Daniel Lim <hbt@markhamboards.com>
2016-11-02 00:57:35 -0400

    Update Comments
    Update Comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const Comments = React.createClass({
+// Hello, I am a Comment!
 
+const Comments = React.createClass({
   renderComment(comment, i) {
-      console.log(comment);
+//       console.log(comment);
+//       console.log(comment.text);
     return (
-        
         <div className="comment" key={ i }>
-        
-            
             <p>
                 <strong>{ comment.user }</strong>
                     { comment.text }
@@ -17,21 +16,18 @@ const Comments = React.createClass({
         </div>
     )
   },
-
   render() {
     return (
       <div className="comments">
         { this.props.postComments.map(this.renderComment) }
-            <form ref="commentForm" className="comment-form" >
-                <input type="text" ref="author" placeholder="author"/>
-                <input type="text" ref="comment" placeholder="comment"/>
+            <form ref="commentForm" className="comment-form">
+                <input type="text" ref="author" placeholder="author" />
+                <input type="text" ref="comment" placeholder="comment" />
                 <input type="submit" hidden />
             </form>
       </div>
     )
   }
 });
-
-// Hello, I am a Comment!
 
 export default Comments;
