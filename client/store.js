bc01f5dc89d15f661eb7c19cf7da7df586d0cca3
Daniel Lim <hbt@markhamboards.com>
2016-11-02 22:53:48 -0400

    Redux Dev Tools ++
    ++ Redux Dev Tools

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore} from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -8,8 +8,13 @@ import posts from './data/posts';
 
 // Create an object for the default data
 const defaultState = { posts, comments };
+const enhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+);
 
-const store = createStore(rootReducer, defaultState);
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(rootReducer, defaultState, enhancers);
 export const history = syncHistoryWithStore(browserHistory, store);
 
 if(module.hot) {
